38ac411a21bb6622f1e1fa16896913e123542413
Orlandoprieto <orlandoprieto19@gmail.com>
2023-01-06 20:41:09 -0300

    se realizo logica de jugadores, marcar y limpiar tablero

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
-const btn = document.getElementById("btn").value;
+const Jugador = (marca, valor) => {
+    let getMarca = marca;
+    let getvalor = valor;
+    let victorias = 0;
+    let esElGanador = false;
 
-console.log(btn)
+    const sumarVictoria = () => {
+        if (victorias == 2) {
+            return esElGanador = true;
+        } else {
+            return victorias++;
+        }
+    }
+
+    return {
+        getMarca,
+        getvalor,
+        sumarVictoria
+    }
+}
+
+const tablero = () => {
+    const tablero = document.getElementById("tablero");
+    let casilleros = Array.from(tablero.children);
+
+    let id = 0;
+    casilleros.forEach(casillero => {
+        casillero.setAttribute("id", id++)
+    })
+
+    const agregarMarca = (marca, valor) => {
+        casilleros.forEach(casillero => {
+            casillero.addEventListener("click", function () {
+                document.getElementsByName(this.id).value = valor;
+                let img = document.getElementById(this.id).children;
+                img[0].src = marca;
+            })
+        })
+    }
+
+    const limpiarTablero = () => {
+        casilleros.forEach(casillero => {
+            casillero.value = "";
+            casillero.children[0].src = "";
+            console.log(casillero.value)
+        })
+    }
+
+    return {
+        agregarMarca,
+        limpiarTablero
+    }
+}
+
+const controladorJuego = (() => {
+    const jugadorUno = Jugador("./img/cruz.svg", "x")
+    const { agregarMarca, limpiarTablero } = tablero();
+
+    agregarMarca(jugadorUno.getMarca, jugadorUno.getvalor);
+
+    setInterval( limpiarTablero , 5000)
+
+})()
