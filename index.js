210b47bc855521b5ae3b1275f6258ba0f5e1672e
Orlando Prieto <orlandoprieto19@gmail.com>
2023-01-11 10:57:50 -0300

    controlador de juego terminado sin IA.

--- index.js
+++ index.js
@@ -1,63 +1,96 @@
-const Jugador = (marca, valor) => {
-    let getMarca = marca;
-    let getvalor = valor;
-    let victorias = 0;
-    let esElGanador = false;
-
-    const sumarVictoria = () => {
-        if (victorias == 2) {
-            return esElGanador = true;
-        } else {
-            return victorias++;
-        }
-    }
-
-    return {
-        getMarca,
-        getvalor,
-        sumarVictoria
-    }
-}
-
-const tablero = () => {
+const tableroDOM = () => {
     const tablero = document.getElementById("tablero");
     let casilleros = Array.from(tablero.children);
 
     let id = 0;
     casilleros.forEach(casillero => {
-        casillero.setAttribute("id", id++)
+        casillero.setAttribute("id", id++);
     })
 
-    const agregarMarca = (marca, valor) => {
-        casilleros.forEach(casillero => {
-            casillero.addEventListener("click", function () {
-                document.getElementsByName(this.id).value = valor;
-                let img = document.getElementById(this.id).children;
-                img[0].src = marca;
-            })
-        })
+    const ocuparCasillero = (casillero, {img, valor}) => {
+        casillero.value = valor;
+        casillero.children[0].src = img;
     }
 
-    const limpiarTablero = () => {
+    const limpiar = () => {
         casilleros.forEach(casillero => {
             casillero.value = "";
             casillero.children[0].src = "";
-            console.log(casillero.value)
         })
     }
 
     return {
-        agregarMarca,
-        limpiarTablero
+        casilleros,
+        ocuparCasillero,
+        limpiar
     }
 }
 
+
+const verificarSiHayGanador = ( {valor} ) => {
+
+    const { casilleros } = tableroDOM();
+
+    const direcciones = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let direccion of direcciones) {
+        if (casilleros[direccion[0]].value == valor && casilleros[direccion[1]].value == valor && casilleros[direccion[2]].value == valor) {
+            return true;
+        }
+    }
+}
+
+
 const controladorJuego = (() => {
-    const jugadorUno = Jugador("./img/cruz.svg", "x")
-    const { agregarMarca, limpiarTablero } = tablero();
+    
+    const { casilleros, limpiar, ocuparCasillero } = tableroDOM();
 
-    agregarMarca(jugadorUno.getMarca, jugadorUno.getvalor);
+    const jugadorUno = {
+        img: "./img/cruz.svg",
+        valor: "x", 
+    }
 
-    setInterval( limpiarTablero , 5000)
+    const jugadorDos = {
+        img: "/img/circulo.svg",
+        valor: "0", 
+    }
 
+    let turno = jugadorUno;
+    let ronda = 0;
+    casilleros.forEach(casillero => {
+        casillero.onclick = function () {
+
+            
+
+            const casillero = document.getElementById(this.id);
+
+            if (casillero.value == "") {
+                ronda++;
+                
+                ocuparCasillero(casillero, turno);
+
+                if (verificarSiHayGanador(turno) ) { 
+                    Swal.fire(`${turno.valor} es el ganador de la ronda`);
+                    limpiar();       
+                    ronda = 0; 
+                    turno.victorias++;
+
+                } else if (ronda == 9) {
+                    Swal.fire(`Esta ronda termino en empate`);
+                    limpiar();
+                } 
+                    
+                turno = (turno == jugadorUno) ? jugadorDos : jugadorUno;
+            }
+        }
+    })
 })()
